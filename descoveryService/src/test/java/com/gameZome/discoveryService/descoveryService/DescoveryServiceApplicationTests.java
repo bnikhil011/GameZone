665d96fd8abdb5061357506093beb597554d2716
Nikhil Bhardwaj <nikhilbhardwaj@Nikhils-MacBook-Air.local>
2022-10-08 23:00:26 +0530

    Added Service Discovery with Modelversion.001

---
+++ descoveryService/src/test/java/com/gameZome/discoveryService/descoveryService/DescoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gameZome.discoveryService.descoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gamezone_models/src/main/java/com/gamezone/models/gamezone_models/Room.java
@@ -0,0 +1,11 @@
+package com.gamezone.models.gamezone_models;
+
+import java.util.List;
+
+public class Room {
+	
+	private int roomId;
+	private List<User> users;
+	
+
+}
