42900972397c71ee892726771416924990295a3b
Nikhil Bhardwaj <nikhilbhardwaj@Nikhils-MacBook-Air.local>
2022-10-09 21:49:29 +0530

    gatway with Auth Service embaded

---
+++ Gateway/src/main/java/com/gameZome/gateway/Gateway/MicroserviceAndUserDetails.java
@@ -0,0 +1,7 @@
+package com.gameZome.gateway.Gateway;
+
+public class MicroserviceAndUserDetails {
+	public static String[] authorizedservices = {"mc1","mc2","mc3","mc4"};
+	public static String[] authorizedsusers = {"Nikhil","Rahul","Umesh","Ankur"};
+
+}
